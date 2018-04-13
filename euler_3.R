b108b11ea4f344b2e911aa08193dd31db628f1a0
Alastair Harry <alastair.harry@gmail.com>
2018-04-13 23:31:37 +0800

    euler3

--- euler_3.R
+++ euler_3.R
@@ -1,6 +1,8 @@
+# R (My solution)
 
+euler_3 <- function(Y){
 x = 3
-end <- 600851475143
+y <- Y
 run = TRUE
 
 prime_check <- function(x){
@@ -18,18 +20,18 @@ prime_check <- function(x){
 
 while (run == TRUE) {
 
-  # First test: is x a factor of `end`?
-  if (end %% x == 0) {
+  # First test: is x a factor of `y`?
+  if (y %% x == 0) {
     # Yes. Second test: is x a prime?
     # Fermats little theorem
     if (prime_check(x) == TRUE) {
-      # Yes. Keep it and divide end by x
-      end <- end / x
+      # Yes. Keep it and divide y by x
+      y <- y / x
       maxprime <- x
       print(maxprime)
-      # Third test: is end equal to one
+      # Third test: is y equal to one
       # Yes. Stop algorithm
-      if (end == 1) {
+      if (y == 1) {
         run <- FALSE
       # No. Go to next possible integer, keep running
         } else {
@@ -46,5 +48,55 @@ while (run == TRUE) {
   
 }
 
+}
 
+euler_3(600851475143)
+
+microbenchmark::microbenchmark(euler_3(600851475143))
+
+# Unit: milliseconds
+# expr      min       lq    mean   median      uq      max neval
+# euler_3(600851475143) 1.638255 1.830261 2.17057 1.975462 2.23208 5.211348   100
+
+# R (Project Euler solution)
+
+euler_3pe <- function(N){
+
+n = N
+
+if(n %% 2 == 0){
+  last_factor = 2
+  n = n / 2
+  while(n %% 2 == 0){
+    n = n / 2
+  }
+} else {
+  last_factor = 1
+}
+
+factor = 3
+max_factor = sqrt(n)
+
+while(n > 1 & factor < max_factor){
+  if(n %% factor == 0){
+    n = n / factor
+    last_factor = factor
+    while(n %% factor == 0){
+      n = n / factor
+    }
+    max_factor = sqrt(n)
+  }
+  factor = factor + 2
+}
+if(n == 1){
+  return(last_factor)}
+    else{return(n)}
+}
+
+
+microbenchmark::microbenchmark(euler_3pe(600851475143))
+
+# Unit: microseconds
+# expr     min      lq     mean  median       uq     max neval
+# euler_3pe(600851475143) 195.432 203.909 208.1393 206.398 210.2045 237.367   100
 
