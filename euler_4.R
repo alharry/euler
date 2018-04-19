15c476fda2756fa908a5d51fd02271241829b148
Alastair Harry <alastair.harry@gmail.com>
2018-04-19 23:16:05 +0800

    euler_4

--- euler_4.R
+++ euler_4.R
@@ -1,17 +1,81 @@
 # R (My solution)
 
-# 
-x <- 100:999
+euler_4 <- function(){
+x <- 999:100
 
 y <- x %*% t(x)
 
 y_000001 <- y %% 10
-y_000010 <- round(y/10) %% 10
-y_000100 <- round(y/100) %% 10
-y_001000 <- round(y/1000) %% 10
-y_010000 <- round(y/1000) %% 10
-y_100000 <- round(y/10000)
+y_000010 <- trunc(y/10) %% 10
+y_000100 <- trunc(y/100) %% 10
+y_001000 <- trunc(y/1000) %% 10
+y_010000 <- trunc(y/10000) %% 10
+y_100000 <- trunc(y/100000) 
 
-y[y_100000 == y_000001 & y_010000 == y_000010 & y_001000 == y_000100]
+max(y[y_100000 == y_000001 & y_010000 == y_000010 & y_001000 == y_000100])
+
+}
+
+euler_4()
+
+# R (PE solution)
+
+euler_4pe <- function(){
+
+  reverse <- function(n){
+    reversed = 0
+    while(n > 0) {
+      reversed = 10 * reversed + n %% 10
+      n = trunc(n/10)
+      }
+    return(reversed)
+    }
+      
+is_palindrome<- function(n){
+  reverse(n) == n
+  }
+
+largest_palindrome = 0
+a = 999
+
+while (a >= 100){
+  if (a %% 11 == 0){
+  b = 999
+  db = 1
+  }else{
+  b = 990 #The largest number less than or equal 999 and divisible by 11
+  db = 11 
+  }
+
+  while (b >= a){
+  if (a*b <= largest_palindrome){
+      break}
+  if (isPalindrome(a*b)){
+    largest_palindrome = a*b}
+  
+  b = b-db
+  }
+  a = a-1
+}
+
+
+return(largest_palindrome)
+}
+
+# Save results
+library(tidyverse)
+library(microbenchmark)
+
+results <- microbenchmark(`4-r-Mine` = euler_4(),
+                          `4-r-PE` = euler_4pe(),
+                          times = 1000,
+                          unit = "us")
+
+data <- data_frame(problem = results$expr, time = results$time) %>%
+  separate(problem, into = c("problem", "lang", "code"), sep = "-")
+
+
+read_rds("data.rds") %>%
+  full_join(data) %>% write_rds("data.rds")
 
 
