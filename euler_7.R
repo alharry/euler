6b4e40506ae4d766203d3dd4c0d66ee85a52823e
Alastair Harry <alastair.harry@gmail.com>
2018-08-20 22:02:14 +0800

    euler_7

--- euler_7.R
+++ euler_7.R
@@ -38,3 +38,47 @@ return(list(j = j, x = x))
 }
 
 euler_7(10001)
+
+# R (Project Euler solution)
+
+euler_7pe <- function(LIMIT){
+  
+isPrime <- function(n){  
+  if(n == 1) {out = FALSE} else {
+    if(n < 4) {out = TRUE} else {
+      if(n %% 2 == 0) {out = FALSE} else {
+        if(n < 9) {out = TRUE} else{
+          if(n %% 3 == 0) {out = FALSE} else {
+            r = floor(sqrt(n))
+            f = 5
+            while(f <= r){
+              if(n %% f == 0){
+                out = FALSE
+                break()} else
+                  if(n %% (f+2) == 0) {
+                    out = FALSE
+                    break()
+                  }
+              f = f + 6
+              out = TRUE
+            }
+          }
+        }
+      }
+    }
+  }
+  
+  return(out)
+}
+
+limit = LIMIT
+count = 1 
+candidate = 1
+
+repeat{candidate = candidate+2
+if(isPrime(candidate)){count = count + 1}
+if(count == limit){break()}
+}
+return(candidate)
+}
+      
